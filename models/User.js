d69315320727c8591c7dae4d3a8ffd921ddcdbd1
yaseminvatan <yvatan035@gmail.com>
2024-10-02 21:08:57 -0700

    imported necessary librarys and connections

---
+++ models/User.js
@@ -0,0 +1,4 @@
+const { Model, Datatypes }= require('sequelize'); // import Model and Datatypes classes from Sequelize.
+const bcrypt = require ( 'bcrypt' ); // import bcrypt library for hashing and comparing passwords
+const sequelize = require('../config/connection'); // imports database connections 
+
