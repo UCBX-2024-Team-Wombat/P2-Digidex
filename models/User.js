4cb7c01a49d7a49b80f3236c9cba4c76473645e4
yaseminvatan <yvatan035@gmail.com>
2024-10-02 21:16:49 -0700

    User class defined ( extends sequelize's Model class)

--- models/User.js
+++ models/User.js
@@ -2,3 +2,9 @@ const { Model, Datatypes }= require('sequelize'); // import Model and Datatypes 
 const bcrypt = require ( 'bcrypt' ); // import bcrypt library for hashing and comparing passwords
 const sequelize = require('../config/connection'); // imports database connections 
 
+// Define class user extend model to represent a table in database  user sinifi artik bir tabloda temsil ediliyo database de 
+class User extends Model {
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginpw, this.pasword);
+    }
+}
