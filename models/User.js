8b887f884ead891773be6d51b66c8323388cfdf0
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-10-03 19:52:53 -0700

    Feature/user model (#2)
    * imported necessary librarys and connections
    
    * User class defined ( extends sequelize's Model class)
    
    * user class definesd
    
    * user table definitian finished
    
    * deletethe extra js files
    
    * exported the user

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Model, Datatypes }= require('sequelize'); // import Model and Datatypes classes from Sequelize.
+const bcrypt = require ( 'bcrypt' ); // import bcrypt library for hashing and comparing passwords
+const sequelize = require('../config/connection'); // imports database connections 
+
+// Define class user extend model to represent a table in database  
+class User extends Model {
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginpw, this.pasword);
+    }
+}
+// user table initialize
+// user id, user name, user password and user email defined.
+//constrains are defined
+User.init({
+    id: {
+        type: Datatypes.INTEGER,
+        allownull: false,
+        primaryKey: true,
+        autoIcrement: true,
+   
+    },
+    // depends on our preferences, we can delete it or we can keep it. we will decide later about keeping user name and surname in our database 
+   // name: {
+   //     type: Datatypes.STRING,
+     //   allownull: true,
+            
+   // },
+    email: {
+        type: Datatypes.STRING,
+        allownull: false,
+        unique: true,
+        validate: {
+            isemail: true,
+        }
+    },
+    password: {
+        type: Datatypes.STRING,
+        allownull: false,
+        validate: {
+            len:[8],
+        }
+    },
+
+}
+
+)
+module.exports = User
