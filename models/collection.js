6d214f62b8dbb7b250a456b4c89b48577e86f016
yaseminvatan <yvatan035@gmail.com>
2024-10-03 20:15:47 -0700

    collection.js is done

--- models/collection.js
+++ models/collection.js
@@ -2,21 +2,31 @@ const { Model, Datatypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class collection extends Model{
-    checkPassword (loginPw) {
+class collection extends Model {
+    checkPassword(loginPw) {
         return bcrypt.compareSync(loginPw, this.pasword);
     }
 }
 
-collection.init (
+collection.init(
     {
-     title: {
-        type: Datatypes.STRING
-        
-        
-     },  
-     UserId: {
-       foreignKey: true
-     }
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: Datatypes.STRING,
+            allownull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        }
     }
 )
+module.export = collection
