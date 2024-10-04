5a5dd45837c9a7d9d141bab7e82263e1c38fd857
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-10-03 21:46:49 -0700

    Feature/user controller setup (#4)
    * Import & connect userRoutes, add comments
    
    * Create, import userRoutes
    
    * Create router, import User model, add endpoint division comments

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,11 @@
-const homeRoutes = require('./home-routes');
+// Import routes
 const router = require('express').Router();
+const homeRoutes = require('./home-routes');
+const userRoutes = require('./api/index');
 
+// Connect imported routers to main router
+router.use('/api', userRoutes);
 router.use('/', homeRoutes);
 
+// Export main router for server.js use
 module.exports = router;
