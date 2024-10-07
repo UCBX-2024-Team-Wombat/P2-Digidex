52000b6f2d889008ab67a1b20acdaa4f42be5905
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-10-07 12:54:14 -0700

    added POST login endpoint

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,6 +2,42 @@ const router = require('express').Router();
 const { User } = require('../../models/index');
 
 // Login Endpoint(s)
+// POST login endpoint
+router.post('/login', async (req, res) => {
+    try {
+      const dbUserData = await User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+  
+      if (!dbUserData) {
+        res
+          .status(400).json({ message: 'Incorrect email or password.' });
+        return;
+      }
+  
+      const validPassword = await dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password. Please try again!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.loggedIn = true;
+  
+        res
+          .status(200)
+          .json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });  
 
 
 // Sign-Up Endpoint(s)
