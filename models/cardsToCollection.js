64e034c8998e1a8b3da68db8f44891adab5d9c19
yaseminvatan <yvatan035@gmail.com>
2024-10-07 06:10:31 -0700

    Card to collection juction table defined.

--- models/cardsToCollection.js
+++ models/cardsToCollection.js
@@ -4,5 +4,30 @@ const sequlize = require("../config/connection");
 class CardsToCollection extends Model {} // do we need any constructor ? discuss with group 
 
 CardsToCollection.init(
+    {
+    collectionId:{
+        type: INTEGER,
+        allownull: false,
+        refrences: {
+            model: 'Collections', //target name
+            key: 'id' //primary key of Collection Model
+        },
+       // onDelete: 'CASCADE', // discuss this
+
+
+    },
+    cardId:{
+        type: INTEGER,
+        allownull: false,
+        references: {
+            model: 'Cards', //target table
+            key: 'id', // Cards primary key 
+
+        },
+       // onDelete: 'CASCADE',
+    },
+
     
+}
+
 )
