780b0b708c8384a2da2b656441d8bb0dda54dd68
yaseminvatan <yvatan035@gmail.com>
2024-10-07 16:45:21 -0700

    Import Necssary libraries and models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,8 @@
+//import necessary Libraries and Models 
 const Collections = require ('./Collections');
 const CardsToCollection = require ('./CardsToCollection');
 const User = require ('./User');
+const CollectionToUser = require(./CollectionToUser);
+
+
 
