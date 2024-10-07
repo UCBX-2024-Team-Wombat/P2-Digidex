db91a4f49e4dd53564b503921f1ac130731330ba
yaseminvatan <yvatan035@gmail.com>
2024-10-07 05:54:17 -0700

    Sequlized model extended.

---
+++ models/cardsToCollection.js
@@ -0,0 +1,8 @@
+const { Model, DataTypes} = require('sequlize');
+const sequlize = require("../config/connection");
+
+class CardsToCollection extends Model {} // do we need any constructor ? discuss with group 
+
+CardsToCollection.init(
+    
+)
