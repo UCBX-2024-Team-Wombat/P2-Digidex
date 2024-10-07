4532026a4758a976ea4702d668126097765aaa32
yaseminvatan <yvatan035@gmail.com>
2024-10-07 06:11:01 -0700

    bug foxed, collection to collections.

--- models/Collections.js
+++ models/Collections.js
@@ -2,13 +2,13 @@ const { Model, Datatypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class collection extends Model {
+class Collections extends Model {
     checkPassword(loginPw) {
         return bcrypt.compareSync(loginPw, this.pasword);
     }
 }
 
-collection.init(
+Collections.init(
     {
         id: {
             type: DataTypes.INTEGER,
