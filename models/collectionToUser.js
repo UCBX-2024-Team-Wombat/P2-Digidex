9522d5210a886e13c63320ff9c78cc041ee05196
yaseminvatan <yvatan035@gmail.com>
2024-10-07 06:56:58 -0700

    junction table created. module loadi=ed and table exported

--- models/collectionToUser.js
+++ models/collectionToUser.js
@@ -1,5 +1,29 @@
-const {Model, Datatypes} = require ('sequelize');
+const { Model, Datatypes, INTEGER } = require('sequelize');
 const sequelize = require('../config/connection');
 // const bcyrpt = require ('bcyrpt);
 
+class CollectionToUser extends Model { };
+CollectionToUser.init(
+    {
+        collectionId: {
+            type: INETEGER,
+            allownull: false,
+            refrences: {
+                model: 'Collections', // target model
+                key: 'id', //primary key of Collections
+
+            },
+        },
+        userId: {
+            type: INTEGER,
+            allownull: false,
+            refrences: {
+                model: 'User',
+                key: 'id', // primary key of USer
+            },
+
+        },
+
+    })
+module.exports = CollectionToUser;
 
