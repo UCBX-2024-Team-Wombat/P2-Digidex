4c2ab90e3360833a070955d64f477ec4987a7a30
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-10-07 19:11:50 -0700

    removed tag attribute from cardsData.js

--- seeds/cardsData.js
+++ seeds/cardsData.js
@@ -4,17 +4,14 @@ const cardsdata = [
     { 
         title: "Card 1", 
         description: "Description for Card 1", 
-        tag: "tags here" 
     },
     { 
         title: "Card 2", 
-        description: "Description for Card 2", 
-        tag: "tags here" 
+        description: "Description for Card 2",  
     },
     { 
         title: "Card 3", 
         description: "Description for Card 2", 
-        tag: "tags here" 
     },
 ];
 
