0719bad044ef42452a604b95a97bf956dedd3784
yaseminvatan <yvatan035@gmail.com>
2024-10-07 22:45:16 -0700

    Set up Express router for handling collection routes and import Collection mode

---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+// Yasemin: this part is for collection handlebars edit modal
+const router = require('express').Router(); //import express and create a new Router
+const  { Collection } = require('../../models'); // import Collection model
+
