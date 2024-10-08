96eec924e41be9c0dc68d7d4ed02c832a29e0d0f
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-10-08 11:47:07 -0700

    Change response handling to send response status

--- public/js/login-handler.js
+++ public/js/login-handler.js
@@ -9,14 +9,8 @@ async function handleLogin(credentials) {
         body: JSON.stringify(credentials),
         headers: { 'Content-Type': 'application/json' },
       });
-  
-      const responseParsed = await response.json()
-
-      if (responseParsed.loginSuccessful == true) {
-        // If successful, redirect the browser to the home page
-        document.location.replace('/homepage');
-      } else {
-        alert(responseParsed.message);
-      }
+      
+      // Return status to caller for handling
+      return response.status;
     }
   };
