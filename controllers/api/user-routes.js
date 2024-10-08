3995b1e501bcf14ec79f042b31052451a31e3079
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-10-08 11:46:31 -0700

    Update response for post to /login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,58 +1,50 @@
-const router = require('express').Router();
-const { User } = require('../../models/index');
+const router = require("express").Router();
+const { User } = require("../../models/index");
 
 // Login Endpoint(s)
 // POST login endpoint
-router.post('/login', async (req, res) => {
-    try {
-      const dbUserData = await User.findOne({
-        where: {
-          email: req.body.email,
-        },
-      });
-      console.log(dbUserData);
-      console.log(dbUserData == false);
+router.post("/login", async (req, res) => {
+  try {
+    // Query user by provided email address
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
 
-      if (dbUserData == false) {
-        res
-        .status(200).json({ 
-          loginSuccessful: false,
-          message: 'Incorrect email or password.' });
-        return;
-      }
-      
-      const validPassword = await dbUserData.checkPassword(req.body.password);
-      
-      if (!validPassword) {
-        res
-        .status(200)
-        .json({ 
-          loginSuccessful: false,
-          message: 'Incorrect email or password. Please try again!' });
-        return;
-      }
-      
-      req.session.save(() => {
-        req.session.loggedIn = true;
-        
-        res
-        .status(200)
-        .json({ user: dbUserData,
-          loginSuccessful: true,
-          message: 'You are now logged in!' });
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+    // If no user found, return error
+    if (!dbUserData) {
+      res.status(401).send();
+      return;
     }
-  });  
+    
+    // Check provided password against queried User
+    const validPassword = await dbUserData.checkPassword(req.body.password);
 
+    // If password invalid, return error
+    if (!validPassword) {
+      res.status(401).send();
+      return;
+    }
+
+    // If password valid, save session with loggedIn value and return success
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).send();
+      return;
+    });
+  } 
+  // Catch errors if they occur
+  catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // Sign-Up Endpoint(s)
 
-
 // Logout Endpoint(s)
-
 
 // Export
 
