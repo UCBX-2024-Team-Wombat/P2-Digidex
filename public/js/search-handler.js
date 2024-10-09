15f2431970c73488f1d876a1c9a2129abf7ee1be
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-10-09 16:05:44 -0700

    Separate card and collection queries

--- public/js/search-handler.js
+++ public/js/search-handler.js
@@ -1,5 +1,5 @@
-// Queries passed string data to Card and Collection endpoints
-async function querySearchText(value) {
+// Queries passed string data to Collection endpoints
+async function queryCollectionsFromSearchText(value) {
   const searchPayload = {
     fullString: value,
     parsedWords: value.split(" "),
@@ -7,6 +7,25 @@ async function querySearchText(value) {
   console.log(searchPayload)
 
   const response = await fetch("/api/collection/search", {
+    method: "POST",
+    body: JSON.stringify(searchPayload),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const parsedResponse = await response.json();
+
+  return parsedResponse;
+}
+
+// Queries passed string data to Card endpoints
+async function queryCollectionsFromSearchText(value) {
+  const searchPayload = {
+    fullString: value,
+    parsedWords: value.split(" "),
+  };
+  console.log(searchPayload)
+
+  const response = await fetch("/api/card/search", {
     method: "POST",
     body: JSON.stringify(searchPayload),
     headers: { "Content-Type": "application/json" },
