404957189373386d20e127881e72856e3eb9df9f
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-10-09 13:40:56 -0700

    Apply formatting

--- controllers/api/collection-routes.js
+++ controllers/api/collection-routes.js
@@ -1,42 +1,39 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const { Op } = require("sequelize");
-const { Collection } = require('../../models/index');
+const { Collection, CardToCollection } = require("../../models/index");
 
 // Bradyn
 // ================
 
-
-
 // Jamil
 // ================
 
-router.post('/search', async (req, res) => {
-
+router.post("/search", async (req, res) => {
   try {
-
     const searchPayload = req.body;
-  
-  
+
     const queryResults = await Collection.findAll({
       where: {
         title: {
-          [Op.iLike]: `%${searchPayload.fullString}%`
-        }
-      }
+          [Op.iLike]: `%${searchPayload.fullString}%`,
+        },
+      },
+      include: [{ model: CardToCollection, required: true }],
     });
-  
-    if(queryResults){
-      res.status(200).json(queryResults.map((collection) => collection.get({plain: true})))
+
+    if (queryResults) {
+      res
+        .status(200)
+        .json(
+          queryResults.map((collection) => collection.get({ plain: true }))
+        );
+    } else {
+      res.status(400);
     }
-    else{
-      res.status(400)
-    }
-  }
-  catch (err){
+  } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
-
 });
 
 // Export
