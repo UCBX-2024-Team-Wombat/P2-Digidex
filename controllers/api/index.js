5a69e9e8b89f276250d720c5a45e36dda3d7b214
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-10-08 19:32:00 -0700

    Add collection router

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 // Create main API router and import related routers
 const router = require('express').Router();
 const userRouter = require('./user-routes');
+const collectionRouter = require('./collection-routes')
 
 // Connect imported routers to main router
 router.use('/user', userRouter);
+router.use('/collection', collectionRouter);
 
 // Export main router for use by controllers/index.js
 module.exports = router;
