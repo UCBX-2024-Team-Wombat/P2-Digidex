36bace4ae787367907d37f6199cc87963b84ef21
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-10-09 16:05:00 -0700

    Add card router

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@
 const router = require('express').Router();
 const userRouter = require('./user-routes');
 const collectionRouter = require('./collection-routes')
+const cardRouter = require('./card-routes');
 
 // Connect imported routers to main router
 router.use('/user', userRouter);
 router.use('/collection', collectionRouter);
+router.use('/card', cardRouter);
 
 // Export main router for use by controllers/index.js
 module.exports = router;
