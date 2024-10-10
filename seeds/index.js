36109a49fbb6e11d4d7534a807404e5d95c426f1
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-10-10 13:40:32 -0700

    Add markdown, individualHooks

--- seeds/index.js
+++ seeds/index.js
@@ -30,19 +30,21 @@ const seedDatabase = async () => {
     {
       title: "Poop",
       description:
-        "Wombat poop is naturally shaped like cubes. Link: https://factanimal.com/wombat/",
+        "Wombat poop is *naturally shaped like cubes*. [Link](https://factanimal.com/wombat/)",
     },
     {
       title: "Bio-fluorescence",
       description:
-        "Wombats glow slightly under ultraviolet light. Link: https://factanimal.com/wombat/",
+        "Wombats **glow** slightly under ultraviolet light. [Link](https://factanimal.com/wombat/)",
     },
     {
       title: "Boney Butts",
       description:
-        "Wombats have extremely dense butts made of boney plates and thick cartilage. They use them to plug up their burrows when attacked. Link: https://www.theguardian.com/science/2020/nov/04/wombats-deadly-bums-how-they-use-their-skull-crushing-rumps-to-fight-play-and-flirt",
+        "Wombats have *extremely dense butts* made of boney plates and thick cartilage. They use them to plug up their burrows when attacked. [Link](https://www.theguardian.com/science/2020/nov/04/wombats-deadly-bums-how-they-use-their-skull-crushing-rumps-to-fight-play-and-flirt)",
     },
-  ]);
+  ],
+  {individualHooks: true}
+);
 
   const wombatCollectionToCardData = [];
   
@@ -54,9 +56,6 @@ const seedDatabase = async () => {
   }
 
   const wombatCollectionToWombatFactCards = await CardToCollection.bulkCreate(wombatCollectionToCardData);
-
-  // seedCollections();
-  // seedCards()
 
   process.exit(0);
 };
