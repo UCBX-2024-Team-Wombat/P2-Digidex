58f9b110a13cb79b4d60ccff4c5a6ed8f9040bd3
yaseminvatan <yvatan035@gmail.com>
2024-10-09 20:39:07 -0700

    mplement route to fetch and render collections for logged-in user, with error handling and session validation

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const withAuth = require('../utils/auth');
-
+const {Collection} = require('../models/index');
 const router = require('express').Router();
 
 router.get('/login', (req, res) => {
@@ -10,7 +10,36 @@ router.get('/', withAuth, (req, res) => {
   res.render('homepage');
 })
 
-router.get('/collections', (req, res) => {
-  res.render('collections-dashboard');
+router.get('/collections', async (req, res) => {
+  try {
+
+    console.log(req.session);
+
+    const queriedCollections = await Collection.findAll({
+      where: {user_id: req.session.user_id}
+    })
+
+    const tmp = await Collection.findAll();
+
+    console.log('tmp')
+    console.log('=======================')
+    console.log(tmp);
+    console.log('=======================')
+
+    console.log(queriedCollections);
+
+    if(queriedCollections){
+      res.render('collections-dashboard',{collections: queriedCollections});
+    }
+    else {
+      res.status(400)
+    }
+
+
+  } catch (error) {
+    console.log(error)
+    res.status(500)
+  }
+ 
 })
 module.exports = router;
