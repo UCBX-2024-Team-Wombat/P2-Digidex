68809e0afef8f105ec17356d29cbb5a658bab5ee
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-10-09 21:10:26 -0700

    added title to collection

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,11 @@
 const withAuth = require('../utils/auth');
-const { Card } = require('../models');
+const { Card, Collection } = require('../models/index');
+const { where } = require('sequelize');
 
 const router = require('express').Router();
 
 router.get('/login', (req, res) => {
-  res.render('login', {layout: 'welcome'});
+  res.render('login', { layout: 'welcome' });
 })
 
 router.get('/', withAuth, (req, res) => {
@@ -13,12 +14,27 @@ router.get('/', withAuth, (req, res) => {
 
 //all cards
 //add withAuth when login is working
-router.get('/dashboard', async (req, res) => {
+router.get('/collection/:id', async (req, res) => {
   try {
-    const cards = await Card.findAll()
-    const cardsdata = cards.map(obj => obj.get({plain: true}))
-    //console.log(cardsdata)
-    res.render('cards-dashboard', {cards: cardsdata});
+    const collectionData = await Collection.findByPk(
+      req.params.id,
+      {
+        //get card data associated with collection 
+        include: {
+          model: Card
+        }
+
+
+      }
+    ) 
+    const collection = collectionData.get ({plain: true});
+    console.log("")
+    console.log("")
+    console.log("")
+    console.log("")
+    console.log(collection.cards);
+  
+    res.render('cards-dashboard', { ...collection });
   } catch (error) {
     console.log(error)
     res.status(500).json(error)
